1704988f511ad480fde17d09dec4973826ecd601
Hiqbal05910 <hamza.iqbal.official1@gmail.com>
2022-07-16 04:40:42 +0500

    Customer io create contact

--- CustomerIO.py
+++ CustomerIO.py
@@ -1,14 +1,11 @@
-# CustomerIO-python-integration
-Customer IO enables organizations to use different customer contacts for marketing compaigns
-
-# Import Packages
-```
 import requests
 import json
-```
 
-# Create Contact on Customer IO
-```
+basic_auth = ("Replace CUSTOMERIO_SITE_ID", "Replace CUSTOMERIO_API_KEY")
+headers = {'content-type': 'application/json'}
+CUSTOMER_IO_WEB_URL=https://track.customer.io/api/v1/customers/
+
+
 def create_contact():
     """
     Handles request for create new contact on CustomerIO
@@ -16,4 +13,3 @@ def create_contact():
     user_data = {"email": "Enter Email", "Name": "Enter Name", "phone": "Enter Phone", "role": "Enter Role"}
     response = requests.put(f"{CUSTOMER_IO_WEB_URL}{Enter_user_id}", data=json.dumps(user_data), auth=basic_auth, headers=headers)
     return response
-```
